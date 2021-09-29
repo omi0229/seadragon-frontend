1b70e61ec779c0859b963847bcf010dff22b24c2
Josh <aya11111@msn.com>
2021-09-30 00:06:25 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+
+<div style="height: 25vmin; text-align: center; display: flex; justify-content: center; align-items: center;">
+    <img src="/S__111558660.jpg" style="height: 22vmin;" />
+</div>
+
+</template>
+
+<script>
+export default {}
+</script>
