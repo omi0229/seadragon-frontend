9be59b4d5dc6cf862ae5247e5c8891506ee2444e
Josh <aya11111@msn.com>
2021-10-07 23:45:06 +0800

    大圖輪播 建構

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
 
     <div>
-        <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="animation: push; max-height: 600">
+        <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="animation: push; max-height: 600" v-show="list.length > 0">
             <ul class="uk-slideshow-items">
-                <li>
-                    <img src="images/banner1.jpg" alt="" uk-cover>
-                </li>
-                <li>
-                    <img src="images/banner2.jpg" alt="" uk-cover>
+                <li v-for="item in list">
+                    <template v-if="item.href">
+                        <a :href="item.href" :target="item.target === 1 ? '_blank' : '_top'" class="uk-text-decoration-none">
+                            <img :src="item.web_img_path" alt="" uk-cover>
+                        </a>
+                    </template>
+                    <template v-else>
+                        <img :src="item.web_img_path" alt="" uk-cover>
+                    </template>
                 </li>
             </ul>
             <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
@@ -24,11 +28,16 @@
         layout: 'default',
         data() {
             return {
-                data: '',
+                list: [],
             }
         },
-        asyncData({$axios}) {
-
+        asyncData({$axios, store, route}) {
+            let api = process.env.API_URL + '/api/banners/list';
+            return $axios.get(api).then(res => {
+                return {
+                    list: res.data,
+                };
+            })
         },
         async fetch ({ $axios, store, params }) {
             await init($axios, store);
