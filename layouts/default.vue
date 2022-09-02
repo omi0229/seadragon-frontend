d96c8070ff2ce1d6ef4faae37db34d57dab1057f
Josh <aya11111@msn.com>
2022-09-02 17:26:55 +0800

    update: GA #3

--- layouts/default.vue
+++ layouts/default.vue
@@ -39,6 +39,14 @@
                     }
                 };
             }
+
+          // Google Analyist
+          window.dataLayer = window.dataLayer || [];
+          const gtag = () => {
+            dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+          gtag('config', 'G-DPSDE8NPMJ');
         }
     }
 </script>
