2980e0c68d36a9a7b2b972b252cbbd2da1482657
Josh <aya11111@msn.com>
2022-09-03 10:59:19 +0800

    update: GA #5

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/app.js',
-    {src: '~/plugins/ga.js', mode: 'client'},
+    '~/plugins/gtag.js',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
